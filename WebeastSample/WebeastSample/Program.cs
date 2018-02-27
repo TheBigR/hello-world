8467a46b58938cb3a15d33c90fae73a67983d14c
TheBigR <36882185+TheBigR@users.noreply.github.com>
2018-02-27 21:29:23 +0200

    just a simple loop

---
+++ WebeastSample/WebeastSample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebeastSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int Sum = 1;
+            int counter = 0;
+
+            for (int i = 1; i < 43; i++)
+			{
+                counter++;
+                Sum = Sum + i;
+			}
+
+            Console.WriteLine("Number of times looped: " + counter);
+            Console.WriteLine("The sum of all numbers is: " + Sum);
+
+           
+
+        }
+    }
+}
